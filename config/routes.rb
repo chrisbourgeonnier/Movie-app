9e43df3dacff7e4d7723ffeda92188e37106a274
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2024-01-19 18:48:43 +0700

    completed and improved navigation over the original Codecademy project

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-  get "movies", to: "movies#index"
+  get "/movies", to: "movies#index"
 
   get "/movies/:id", to: "movies#show", as: :movie
+
+  get "/actors", to: "actors#index"
+
+  get "/actors/:id", to: "actors#show", as: :actor
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
